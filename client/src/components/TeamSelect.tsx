4f9be61aca7c4ca3c49a29aad3f9b56157dcc46f
Carlisle <carlisle.m@hotmail.com>
2021-07-03 22:15:27 +1000

    .

--- client/src/components/TeamSelect.tsx
+++ client/src/components/TeamSelect.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import Select from 'react-dropdown-select'
 import RootStore from '../store'
 import { mapTeamName } from '../helpers/mapTeamNames'
-import { useState } from 'react'
+import { Observer } from 'mobx-react'
 
 const TeamSelectContainer = styled.div`
 	display: flex;
@@ -35,22 +35,6 @@ interface Props {
 }
 
 const TeamSelect = (props: Props) => {
-	// const [selectValues, setSelectValues] = useState<any>([])
-
-	// const onChange = (values: any) => {
-	// 	setSelectValues(values)
-	// }
-
-	// const currentTeam = [{ value: props.selectedTeam, label: props.selectedTeam }]
-
-	// props.selectedTeam && setSelectValues({ value: props.selectedTeam, label: props.selectedTeam })
-
-	// const onSet = (values: any) => {
-	// 	const newValue = values.map((val: any) => ({ value: 'xxx', label: 'xxx' }))
-
-	// 	setSelectValues(newValue)
-	// }
-
 	return (
 		<TeamSelectContainer>
 			{props.selectedTeam !== null ? (
@@ -76,48 +60,35 @@ const TeamSelect = (props: Props) => {
 				</TeamLogo>
 			)}
 
-			<TeamSelectDropdown>
-				{/* <div
-					style={{ height: 200, width: 200, backgroundColor: 'red' }}
-					onClick={() => {
-						console.log('clicked')
-						console.log('selectedValues: ', selectValues)
-						onSet(['test'])
-					}}
-				/> */}
-				<Select
-					style={{ width: 280, maxHeight: 540 }}
-					options={
-						// [
-						// 	{ value: 'test', label: 'test' },
-						// 	{ value: 'test2', label: 'test2' },
-						// 	{ value: 'tset3', label: 'tset3' },
-						// 	{ value: 'test4', label: 'test4' },
-						// ]
-						props.teams !== null && RootStore.selectedLeague !== null
-							? props.teams
-									.filter(
-										(team: any) => team.league_id === RootStore.selectedLeague
-									)
-									.map((team: any) => {
-										return { value: team.name, label: team.name }
-									})
-							: []
-					}
-					searchable={true}
-					keepSelectedInList={false} // Test
-					values={
-						[...[{ value: props.selectedTeam, label: props.selectedTeam }]]
-						// [{ value: props.selectedTeam, label: props.selectedTeam }]
-					}
-					onChange={(value: any) => {
-						// onChange(value)
-						mapTeamName(value[0].value) && props.teamNumber === 1
-							? RootStore.updateSelectedTeamOne(mapTeamName(value[0].value))
-							: RootStore.updateSelectedTeamTwo(mapTeamName(value[0].value))
-					}}
-				/>
-			</TeamSelectDropdown>
+			<Observer>
+				{() => (
+					<TeamSelectDropdown>
+						<Select
+							style={{ width: 280, maxHeight: 540 }}
+							options={
+								props.teams !== null && RootStore.selectedLeague !== null
+									? props.teams
+											.filter(
+												(team: any) =>
+													team.league_id === RootStore.selectedLeague
+											)
+											.map((team: any) => {
+												return { value: team.name, label: team.name }
+											})
+									: []
+							}
+							searchable={true}
+							keepSelectedInList={false} // Test
+							values={[...props.selectedTeam]}
+							onChange={(value: any) => {
+								mapTeamName(value[0].value) && props.teamNumber === 1
+									? RootStore.updateSelectedTeamOne(mapTeamName(value[0].value))
+									: RootStore.updateSelectedTeamTwo(mapTeamName(value[0].value))
+							}}
+						/>
+					</TeamSelectDropdown>
+				)}
+			</Observer>
 		</TeamSelectContainer>
 	)
 }
