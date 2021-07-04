db2fa5f0150530b164d967ad6ee43804304a50bc
Carlisle <carlisle.m@hotmail.com>
2021-07-04 23:16:41 +1000

    .

--- client/src/components/MatchScheduleBar.tsx
+++ client/src/components/MatchScheduleBar.tsx
@@ -94,89 +94,97 @@ const MatchScheduleBar = (props: Props) => {
 
 		return (
 			<>
-				{props.upcomingGames !== null &&
-				props.upcomingGames.filter(
-					(upcomingGame: any) =>
-						upcomingGame.league_id === RootStore.selectedLeague
-				).length > 0 ? (
-					props.upcomingGames
-						.filter(
-							(upcomingGame: any) =>
-								upcomingGame.league_id === RootStore.selectedLeague
-						)
-						.map((match: any, matchIndex: number) => (
-							<>
-								{matchIndex === 0 ? (
-									<MatchWeek>Week {match.match_week}</MatchWeek>
-								) : (
-									props.upcomingGames.filter(
+				<Observer>
+					{() => (
+						<>
+							{props.upcomingGames !== null &&
+							props.upcomingGames.filter(
+								(upcomingGame: any) =>
+									upcomingGame.league_id === RootStore.selectedLeague
+							).length > 0 ? (
+								props.upcomingGames
+									.filter(
 										(upcomingGame: any) =>
 											upcomingGame.league_id === RootStore.selectedLeague
-									)[matchIndex]!.match_week >
-										props.upcomingGames.filter(
-											(upcomingGame: any) =>
-												upcomingGame.league_id === RootStore.selectedLeague
-										)[matchIndex - 1]!.match_week && (
-										<MatchWeek>Week {match.match_week}</MatchWeek>
 									)
-								)}
-								<MatchSchedule
-									key={matchIndex}
-									style={{
-										borderBottom:
-											matchIndex !==
-											props.upcomingGames.filter(
-												(upcomingGame: any) =>
-													upcomingGame.league_id === RootStore.selectedLeague
-											).length -
-												1
-												? props.upcomingGames.filter(
+									.map((match: any, matchIndex: number) => (
+										<>
+											{matchIndex === 0 ? (
+												<MatchWeek>Week {match.match_week}</MatchWeek>
+											) : (
+												props.upcomingGames.filter(
+													(upcomingGame: any) =>
+														upcomingGame.league_id === RootStore.selectedLeague
+												)[matchIndex]!.match_week >
+													props.upcomingGames.filter(
 														(upcomingGame: any) =>
 															upcomingGame.league_id ===
 															RootStore.selectedLeague
-												  )[matchIndex].match_week <
-												  props.upcomingGames.filter(
-														(upcomingGame: any) =>
-															upcomingGame.league_id ===
-															RootStore.selectedLeague
-												  )[matchIndex + 1].match_week
-													? 'none'
-													: '1px solid black'
-												: 'none',
-									}}
-									onClick={() => {
-										RootStore.updateSelectedTeamOne(match.blue_team)
-										RootStore.updateSelectedTeamTwo(match.red_team)
-									}}
-								>
-									<MatchScheduleTeamLogo>
-										{convertDateToPST(match.game_date.split('T')[0]) ===
-											today && <TodayMatch />}
-										{teamLogo(match.blue_team)}
-									</MatchScheduleTeamLogo>
-									<MatchScheduleTime>
-										<span>{mapAbvDayToFull(match.match_day)}</span>
-										<span>{match.match_time} PST</span>
-										<span>
-											{convertDateToPST(match.game_date.split('T')[0])}
-										</span>
-									</MatchScheduleTime>
-									<MatchScheduleTeamLogo>
-										{teamLogo(match.red_team)}
-									</MatchScheduleTeamLogo>
-								</MatchSchedule>
-							</>
-						))
-				) : (
-					<Placeholder>
-						<QuestionCircleOutlined
-							style={{ fontSize: 100, paddingBottom: 10 }}
-						/>
-						<span style={{ fontSize: 30 }}>
-							Begin by selecting a league to display upcoming matches
-						</span>
-					</Placeholder>
-				)}
+													)[matchIndex - 1]!.match_week && (
+													<MatchWeek>Week {match.match_week}</MatchWeek>
+												)
+											)}
+											<MatchSchedule
+												key={matchIndex}
+												style={{
+													borderBottom:
+														matchIndex !==
+														props.upcomingGames.filter(
+															(upcomingGame: any) =>
+																upcomingGame.league_id ===
+																RootStore.selectedLeague
+														).length -
+															1
+															? props.upcomingGames.filter(
+																	(upcomingGame: any) =>
+																		upcomingGame.league_id ===
+																		RootStore.selectedLeague
+															  )[matchIndex].match_week <
+															  props.upcomingGames.filter(
+																	(upcomingGame: any) =>
+																		upcomingGame.league_id ===
+																		RootStore.selectedLeague
+															  )[matchIndex + 1].match_week
+																? 'none'
+																: '1px solid black'
+															: 'none',
+												}}
+												onClick={() => {
+													RootStore.updateSelectedTeamOne(match.blue_team)
+													RootStore.updateSelectedTeamTwo(match.red_team)
+												}}
+											>
+												<MatchScheduleTeamLogo>
+													{convertDateToPST(match.game_date.split('T')[0]) ===
+														today && <TodayMatch />}
+													{teamLogo(match.blue_team)}
+												</MatchScheduleTeamLogo>
+												<MatchScheduleTime>
+													<span>{mapAbvDayToFull(match.match_day)}</span>
+													<span>{match.match_time} PST</span>
+													<span>
+														{convertDateToPST(match.game_date.split('T')[0])}
+													</span>
+												</MatchScheduleTime>
+												<MatchScheduleTeamLogo>
+													{teamLogo(match.red_team)}
+												</MatchScheduleTeamLogo>
+											</MatchSchedule>
+										</>
+									))
+							) : (
+								<Placeholder>
+									<QuestionCircleOutlined
+										style={{ fontSize: 100, paddingBottom: 10 }}
+									/>
+									<span style={{ fontSize: 30 }}>
+										Begin by selecting a league to display upcoming matches
+									</span>
+								</Placeholder>
+							)}
+						</>
+					)}
+				</Observer>
 			</>
 		)
 	}
