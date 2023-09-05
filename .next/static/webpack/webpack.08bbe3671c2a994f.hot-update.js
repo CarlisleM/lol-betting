26641e6c3637a20e5d0f1d07debc6a3b3f43f859
Carlisle Miller <carlisle.m@hotmail.com>
2023-09-05 03:29:31 -0700

    test build fix

--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "fd09d8bafe8d984c"; }
+/******/ 	__webpack_require__.h = function() { return "3af03c1d3e963b88"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
@@ -723,6 +723,11 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 	var chunkLoadingGlobal = self["webpackChunk_N_E"] = self["webpackChunk_N_E"] || [];
 /******/ 	chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
 /******/ 	chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ }();
+/******/ 
+/******/ /* webpack/runtime/nonce */
+/******/ !function() {
+/******/ 	__webpack_require__.nc = undefined;
 /******/ }();
 /******/ 
 /******/ }
